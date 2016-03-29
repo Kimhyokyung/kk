40ee7b7f4e148e98fb72658ab9556600feef8732
junhan <junhan900602@naver.com>
2016-03-29 15:46:41 +0900

    mongo Commit

--- src/main/java/com/bit/kuku/vo/ChatVo.java
+++ src/main/java/com/bit/kuku/vo/ChatVo.java
@@ -4,53 +4,82 @@ import java.text.SimpleDateFormat;
 
 public class ChatVo {
 
-	private int idx;
+	private String chatroom_num;
 	private String Sender_email;
 	private String receiver_email;
 	private String chat;
-	private SimpleDateFormat time;
-	private boolean receiver_response;
+	private String time;
+	private String receiver_response;
 	
-	public int getIdx() {
-		return idx;
-	}
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-	public String getSender_email() {
-		return Sender_email;
-	}
-	public void setSender_email(String sender_email) {
-		Sender_email = sender_email;
-	}
-	public String getReceiver_email() {
-		return receiver_email;
-	}
-	public void setReceiver_email(String receiver_email) {
-		this.receiver_email = receiver_email;
-	}
-	public String getChat() {
-		return chat;
-	}
-	public void setChat(String chat) {
-		this.chat = chat;
-	}
-	public SimpleDateFormat getTime() {
-		return time;
-	}
-	public void setTime(SimpleDateFormat time) {
-		this.time = time;
-	}
-	public boolean isReceiver_response() {
-		return receiver_response;
-	}
-	public void setReceiver_response(boolean receiver_response) {
-		this.receiver_response = receiver_response;
-	}
+	public ChatVo() {
+		  super();
+		  // TODO Auto-generated constructor stub
+		 }
+
+		 public ChatVo(String chatroom_num, String Sender_email, String receiver_email, String chat,
+				 String time, String receiver_response) {
+		  super();
+		  this.chatroom_num = chatroom_num;
+		  this.Sender_email = Sender_email;
+		  this.receiver_email = receiver_email;
+		  this.chat = chat;
+		  this.time = time;
+		  this.receiver_response=receiver_response;
+		 }
 	
+	public String getChatroom_num() {
+			return chatroom_num;
+		}
+
+		public void setChatroom_num(String chatroom_num) {
+			this.chatroom_num = chatroom_num;
+		}
+
+		public String getSender_email() {
+			return Sender_email;
+		}
+
+		public void setSender_email(String sender_email) {
+			Sender_email = sender_email;
+		}
+
+		public String getReceiver_email() {
+			return receiver_email;
+		}
+
+		public void setReceiver_email(String receiver_email) {
+			this.receiver_email = receiver_email;
+		}
+
+		public String getChat() {
+			return chat;
+		}
+
+		public void setChat(String chat) {
+			this.chat = chat;
+		}
+
+		public String getTime() {
+			return time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+
+		public String getReceiver_response() {
+			return receiver_response;
+		}
+
+		public void setReceiver_response(String receiver_response) {
+			this.receiver_response = receiver_response;
+		}
+
 	@Override
 	public String toString() {
-		return "ChatVo [idx=" + idx + ", Sender_email=" + Sender_email + ", receiver_email=" + receiver_email
-				+ ", chat=" + chat + ", time=" + time + ", receiver_response=" + receiver_response + "]";
+		return "ChatVo [chatroom_num=" + chatroom_num + ", Sender_email=" + Sender_email + ", receiver_email="
+				+ receiver_email + ", chat=" + chat + ", time=" + time + ", receiver_response=" + receiver_response
+				+ "]";
 	}
+	
 }
